eb8215917f169cee68e14a8e95eea65ace6a1159
guysharony <sharonyguy@gmail.com>
2020-10-20 13:07:33 +0200

    Fixing leaks

---
+++ cpp01/ex01/ex01.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/20 13:06:16 by gsharony          #+#    #+#             */
+/*   Updated: 2020/10/20 13:07:15 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete panthere;
+}
