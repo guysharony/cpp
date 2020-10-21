63f13d57bc9575b453942bf453b6b90f1328808c
guysharony <sharonyguy@gmail.com>
2020-10-21 10:55:36 +0200

    Improving main.cpp

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 14:35:16 by gsharony          #+#    #+#             */
-/*   Updated: 2020/10/21 10:47:28 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/10/21 10:54:43 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,11 @@ int	main(void) {
 	zombieEvent->setZombieType("Chump");
 	zombieEvent->randomChump();
 	delete zombieEvent;
+
+	zombieEvent->setZombieType("Developer");
+	
+	zombie = zombieEvent->newZombie("Alex");
+	zombie->announce();
+	delete zombie;
 	return (0);
 }
