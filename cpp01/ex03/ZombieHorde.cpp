7cc16ffd4acc3dbe06ed0eeddfa8c2bba452693d
guysharony <sharonyguy@gmail.com>
2020-10-22 10:28:17 +0200

    Adding verification for nbr of zombies

--- cpp01/ex03/ZombieHorde.cpp
+++ cpp01/ex03/ZombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 13:06:52 by gsharony          #+#    #+#             */
-/*   Updated: 2020/10/22 10:24:56 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/10/22 10:27:36 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@ ZombieHorde::ZombieHorde(int n) {
 	std::string	str;
 	std::string	tmp;
 	
-	this->_total = n;
-	this->_zombie = new Zombie[n];
-	tmp = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-	for (int i = 0; i < n; i++) {
-		for(int len = 0; len < 5; len++) {
-			str += tmp[rand() % 52];
+	if (n > 0) {
+		this->_total = n;
+		this->_zombie = new Zombie[n];
+		tmp = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+		for (int i = 0; i < n; i++) {
+			for(int len = 0; len < 5; len++) {
+				str += tmp[rand() % 52];
+			}
+			this->_zombie[i].setName(str);
+			this->_zombie[i].setType("Random");
+			str = "";
 		}
-		this->_zombie[i].setName(str);
-		this->_zombie[i].setType("Random");
-		str = "";
+	} else {
+		std::cout << "The number of zombies must be positive!" << std::endl;
 	}
 }
 
