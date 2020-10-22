c589e184d9147f64a79c5e30d17ca9ff4b7f9b8a
guysharony <sharonyguy@gmail.com>
2020-10-22 10:39:53 +0200

    Displaying string with pointer and referance

--- cpp01/ex04/ex04.cpp
+++ cpp01/ex04/ex04.cpp
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 10:30:46 by gsharony          #+#    #+#             */
-/*   Updated: 2020/10/22 10:37:12 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/10/22 10:39:01 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,8 @@ int	main(void) {
 	std::string 	str = "HI THIS IS BRAIN";
 	std::string* 	ptr = &str;
 	std::string& 	ref = str;
+
+	std::cout << "Using pointer    - [" << *ptr << "]" << std::endl;
+	std::cout << "Using referance  - [" << ref << "]" << std::endl;
 	return (0);
 }
