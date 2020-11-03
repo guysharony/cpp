92204b34b38f1dd357f04bc4b057396dd68817b4
guysharony <sharonyguy@gmail.com>
2020-11-03 09:52:50 +0100

    Adding messages

--- cpp03/ex00/FragTrap.cpp
+++ cpp03/ex00/FragTrap.cpp
@@ -6,21 +6,57 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 12:53:39 by gsharony          #+#    #+#             */
-/*   Updated: 2020/11/02 13:58:16 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/11/03 09:51:44 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
 
-FragTrap::FragTrap(void): _hitPoints(100),  _maxHitPoints(100), _energyPoints(100), _level(1), _name(""), _meleeAttackDamage(30), _rangedAttackDamage(20), _armorDamageReduction(5) {
-	
+FragTrap::FragTrap(void)
+:
+	_hitPoints(100),
+	_maxHitPoints(100),
+	_energyPoints(100),
+	_level(1),
+	_name("Hyperion"),
+	_meleeAttackDamage(30),
+	_rangedAttackDamage(20),
+	_armorDamageReduction(5)
+{
+	std::cout << "FR4G-TP " << this->_name << " est en cours d'initialization..." << std::endl;
+	std::cout << "FR4G-TP " << this->_name << " est prêt à être déployé !" << std::endl;
+	return;
 }
 
-FragTrap::FragTrap(std::string name) {}
+FragTrap::FragTrap(std::string name)
+:
+	_hitPoints(100),
+	_maxHitPoints(100),
+	_energyPoints(100),
+	_level(1),
+	_name(name),
+	_meleeAttackDamage(30),
+	_rangedAttackDamage(20),
+	_armorDamageReduction(5)
+{
+	std::cout << "FR4G-TP " << this->_name << " est prêt à être déployé !" << std::endl;
+	return;
+}
 
-FragTrap::FragTrap(FragTrap const & src) { *this = src; }
+FragTrap::FragTrap(FragTrap const & src)
+{
+	*this = src;
+	std::cout << "Déploiement de FR4G-TP " << this->_name << " !" << std::endl;
+	return;
+}
 
-FragTrap::~FragTrap(void) {}
+FragTrap::~FragTrap(void) {
+	if (this->_hitPoints)
+		std::cout << "Félicitation FR4G-TP " << this->_name << ", vous restez vivant jusqu'à la fin de votre mission !" << std::endl;
+	else
+		std::cout << "FR4G-TP " << this->_name << ", vous avez échoué votre mission..." << std::endl;
+	return;
+}
 
 FragTrap & FragTrap::operator=(FragTrap const & src) {
 	return (*this);
