1dfd3cab111677421cc0ca473d004f451e45d495
guysharony <sharonyguy@gmail.com>
2020-11-24 08:25:15 +0100

    Finishing main

--- cpp03/ex03/main.cpp
+++ cpp03/ex03/main.cpp
@@ -6,21 +6,26 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 12:53:47 by gsharony          #+#    #+#             */
-/*   Updated: 2020/11/04 09:20:00 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/11/24 08:19:55 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "FragTrap.hpp"
 # include "ScavTrap.hpp"
+# include "NinjaTrap.hpp"
 
 int main(void) {
 	FragTrap a("Human");
 	ScavTrap b("Terminator");
-    
+    NinjaTrap c("Ninja");
+	
 	std::cout << std::endl;
     a.rangedAttack("Terminator");
 	b.takeDamage(20);
 	b.challengeNewcomer();
+	c.ninjaShoebox(a);
+	c.ninjaShoebox(b);
+	
 	std::cout << std::endl;
 	a.beRepaired(20);
 	b.beRepaired(20);
--- cpp03/ex04/main.cpp
+++ cpp03/ex04/main.cpp
@@ -6,32 +6,36 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/02 12:53:47 by gsharony          #+#    #+#             */
-/*   Updated: 2020/11/19 08:20:44 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/11/24 08:24:41 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "FragTrap.hpp"
 # include "ScavTrap.hpp"
+# include "NinjaTrap.hpp"
 # include "SuperTrap.hpp"
 
 int main(void) {
 	FragTrap a("Human");
 	ScavTrap b("Terminator");
-	SuperTrap c("Michel");
-    
+    NinjaTrap c("Ninja");
+	SuperTrap d("Superman");
+	
 	std::cout << std::endl;
     a.rangedAttack("Terminator");
 	b.takeDamage(20);
 	b.challengeNewcomer();
-	c.rangedAttack("Human");
-	a.takeDamage(20);
-	c.meleeAttack("Human");
-	a.takeDamage(60);
-
+	c.ninjaShoebox(a);
+	c.ninjaShoebox(b);
+	
 	std::cout << std::endl;
 	a.beRepaired(20);
 	b.beRepaired(20);
-
+	d.rangedAttack("Ninja");
+	c.takeDamage(20);
+	d.rangedAttack("Human");
+	a.takeDamage(60);
+	
 	std::cout << std::endl;
 	a.vaulthunter_dot_exe("Terminator");
 	b.takeDamage(25);
@@ -49,11 +53,9 @@ int main(void) {
 	b.takeDamage(25);
 	b.meleeAttack("Human");
 	a.takeDamage(25);
-
 	std::cout << std::endl;
 	a.meleeAttack("Terminator");
 	b.takeDamage(30);
-	
 	std::cout << std::endl;
 	return (0);
 }
