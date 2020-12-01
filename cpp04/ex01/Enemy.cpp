6d563ad7e976f392e603bfc86710d9a0cf48963f
Guy Sharony <sharonyguy@gmail.com>
2020-12-01 09:29:42 +0100

    Adding Enemy

---
+++ cpp04/ex01/Enemy.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Enemy.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 09:11:25 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/01 09:27:53 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Enemy.hpp"
+
+Enemy::Enemy(void)
+{
+    return;
+}
+
+Enemy::Enemy(int hp, std::string const & type)
+:
+    _hp(hp),
+    _type(type)
+{
+    return;
+}
+
+Enemy::~Enemy(void)
+{
+    return;
+}
+
+Enemy & Enemy::operator=(Enemy const & src)
+{
+	this->_hp = src._hp;
+	this->_type = src._type;
+	return (*this);
+}
+
+std::string const   Enemy::getType(void) const {
+    return (_type);
+}
+
+int                 Enemy::getHP(void) const {
+    return (_hp);
+}
+
+void                Enemy::takeDamage(int damage) {
+    if (damage < 0)
+        this->_hp = (this->_hp - damage < 0) ? 0 : this->_hp - damage;
+}
---
+++ cpp04/ex01/Enemy.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Enemy.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/01 09:16:19 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/01 09:28:19 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ENEMY_CLASS_HPP
+# define ENEMY_CLASS_HPP
+
+# include <iostream>
+# include <string>
+
+class Enemy
+{
+    private:
+        Enemy(void);
+        int             _hp;
+        std::string     _type;
+        
+    public:
+        Enemy(int hp, std::string const & type);
+        virtual ~Enemy();
+
+        Enemy & operator=(Enemy const & src);
+
+        std::string const   getType(void) const;
+        int                 getHP(void) const;
+
+        virtual void takeDamage(int);
+};
+
+#endif
