d9a2ea27845df2f8feaf1f8ceb8835cb852d90e3
guysharony <sharonyguy@gmail.com>
2020-12-08 10:26:44 +0100

    Drawing a tree

--- cpp05/ex02/ShrubberyCreationForm.cpp
+++ cpp05/ex02/ShrubberyCreationForm.cpp
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/07 14:01:20 by gsharony          #+#    #+#             */
-/*   Updated: 2020/12/08 09:50:12 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/12/08 10:25:53 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,9 +52,22 @@ void						ShrubberyCreationForm::executeForm(Bureaucrat const & executor) const
 	file.open(this->getName() + "_shrubbery");
 	if (file.is_open())
 	{
-		file << "this is a test 1!" << std::endl;
-		file << "this is a test 2!" << std::endl;
-		file << "this is a test 3!" << std::endl;
+		file << "         ___            " << std::endl;
+		file << "    /---     ---\\      " << std::endl;
+		file << " /--             --\\   " << std::endl;
+		file << "|                   |   " << std::endl;
+		file << "|                   |   " << std::endl;
+		file << "|                   |   " << std::endl;
+		file << " \\   --       --   /    " << std::endl;
+		file << "  -     \\\\  //  -      " << std::endl;
+		file << "      \\\\//  \\\\//      " << std::endl;
+		file << "       \\\\    //       " << std::endl;
+		file << "        ==  ==          " << std::endl;
+		file << "         }}{{           " << std::endl;
+		file << "         }}{{           " << std::endl;
+		file << "         }}{{           " << std::endl;
+		file << "     /----------\\      " << std::endl;
+		file << "____/            \\____ " << std::endl;
 	}
 	file.close();
 	return;
