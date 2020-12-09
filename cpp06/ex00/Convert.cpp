0c02563d674a864403b37bcca2d225f25de3c28c
guysharony <sharonyguy@gmail.com>
2020-12-09 15:16:36 +0100

    Test cpp06

---
+++ cpp06/ex00/Convert.cpp
@@ -0,0 +1,54 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Convert.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 11:01:36 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/09 15:11:36 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Convert.hpp"
+
+Convert::Convert(void)
+:
+	_input(NULL)
+{
+	return;
+}
+
+Convert::Convert(std::string const & input)
+:
+	_input(input)
+{
+	return;
+}
+
+Convert::Convert(Convert const & src)
+{
+	*this = src;
+	return;
+}
+
+Convert::~Convert()
+{
+	return;
+}
+
+/*Convert				&Convert::operator=(Convert const & src)
+{
+	this->_input = src.getInput();
+	return (*this);
+}*/
+
+/*std::string	const	Convert::getInput(void) const
+{
+	return (this->_input);
+}
+
+void				Convert::setInput(std::string const & input)
+{
+	this->_input = input;
+}*/
---
+++ cpp06/ex00/Convert.hpp
@@ -0,0 +1,38 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Convert.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/09 11:02:07 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/09 15:10:57 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONVERT_CLASS_HPP
+# define CONVERT_CLASS_HPP
+
+# include <iostream>
+# include <string>
+# include <fstream>
+# include <sstream>
+
+class Convert
+{
+	public:
+		Convert(void);
+		Convert(std::string const & input);
+		Convert(Convert const & src);
+		~Convert();
+
+		/*Convert				&operator=(Convert const & src);*/
+
+		std::string	const	getInput(void) const;
+		void				setInput(std::string const & input);
+		
+	private:
+		std::string 		_input;
+};
+
+#endif
