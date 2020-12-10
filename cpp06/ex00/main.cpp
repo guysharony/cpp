db7ae01d426a9e58b861b5a11ae2e5bd21f543a3
guysharony <sharonyguy@gmail.com>
2020-12-10 14:23:10 +0100

    Test

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 11:00:45 by gsharony          #+#    #+#             */
-/*   Updated: 2020/12/10 14:12:00 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/12/10 14:22:59 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,12 @@ int		main(int argc, char **argv)
 		std::string 	str(argv[1]);
 		Convert			c(str);
 
-		std::cout << "char: " << c.getChar() << std::endl;
+		std::cout << "char: ";
+		try {
+			std::cout << c.getChar() << std::endl;
+		} catch(const std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
 		std::cout << "int: ";
 		try {
 			std::cout << c.getInt() << std::endl;
