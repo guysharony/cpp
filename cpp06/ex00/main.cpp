fb567c4f35b5e4ff7b729c149e5ef55f8699e85a
guysharony <sharonyguy@gmail.com>
2020-12-10 12:33:55 +0100

    Test 16

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/09 11:00:45 by gsharony          #+#    #+#             */
-/*   Updated: 2020/12/10 12:19:48 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/12/10 12:33:44 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int		main(int argc, char **argv)
 		std::cout << "char: " << c.getChar() << std::endl;
 		std::cout << "int: " << c.getInt() << std::endl;
 		std::cout << "float: " << c.getFloat() << std::endl;
+		std::cout << "double: " << c.getDouble() << std::endl;
 	}
 	return (0);
 }
