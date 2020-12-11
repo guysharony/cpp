e2c256212d3b5e2c3b8e70d035f40b6e491c2550
guysharony <sharonyguy@gmail.com>
2020-12-11 11:12:27 +0100

    Finished ex01

---
+++ cpp07/ex01/iter.hpp
@@ -0,0 +1,43 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 10:37:32 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/11 11:11:36 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+
+template <typename T>
+void	iter(T *array, int length, void (*fct)(T &prms))
+{
+	for (int i = 0; i < length; i++)
+		fct(array[i]);
+}
+
+template <typename T>
+void	decrByFive(T &a)
+{
+	a -= 5;
+}
+
+template <typename T>
+void	change(T &a)
+{
+	for (int i = 0; i < a.length(); i++)
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+			a[i] -= 32;
+		else if (a[i] >= 'A' && a[i] <= 'Z')
+			a[i] += 32;
+	}
+}
+
+#endif
---
+++ cpp07/ex01/main.cpp
@@ -0,0 +1,44 @@
+﻿/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 10:37:10 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/11 11:11:55 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "iter.hpp"
+
+int	main(void)
+{
+	int			numbers[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	std::string strings[3] = {"This iS a tEst", "I d0n't LiKe TeA", "S0Me th1ng Is Wr0ng..."};
+	
+	std::cout << "<=== Decrement By Five ===>" << std::endl;
+	iter(numbers, 10, &decrByFive);
+	std::cout << "[";
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << numbers[i];
+		if (i < 9)
+			std::cout << ", ";
+	}
+	std::cout << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Change Caps ===>" << std::endl;
+	iter(strings, 3, &change);
+	std::cout << "[";
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << strings[i];
+		if (i < 2)
+			std::cout << ", ";
+	}
+	std::cout << "]" << std::endl;
+	return (0);
+}
