1da639b8024ffb34e677ee92aa786ed2036fda77
guysharony <sharonyguy@gmail.com>
2020-12-11 10:32:09 +0100

    Finished ex00

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 10:02:44 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/11 10:30:33 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int		main(void)
+{
+	int		ia = 5;
+	int		ib = 10;
+
+	float	fa = 5.5;
+	float	fb = 10.23;
+
+	char	ca = 'a';
+	char	cb = 'b';
+
+	std::cout << "<=== Swap 1/3 ===>" << std::endl;
+	std::cout << "[ia: " << ia << "]" << std::endl;
+	std::cout << "[ib: " << ib << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[Swapping ia & ib]" << std::endl;
+	swap(ia, ib);
+
+	std::cout << std::endl;
+	std::cout << "[ia: " << ia << "]" << std::endl;
+	std::cout << "[ib: " << ib << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Swap 2/3 ===>" << std::endl;
+	std::cout << "[fa: " << fa << "]" << std::endl;
+	std::cout << "[fb: " << fb << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[Swapping fa & fb]" << std::endl;
+	swap(fa, fb);
+
+	std::cout << std::endl;
+	std::cout << "[fa: " << fa << "]" << std::endl;
+	std::cout << "[fb: " << fb << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Swap 3/3 ===>" << std::endl;
+	std::cout << "[ca: " << ca << "]" << std::endl;
+	std::cout << "[cb: " << cb << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[Swapping ca & cb]" << std::endl;
+	swap(ca, cb);
+
+	std::cout << std::endl;
+	std::cout << "[ca: " << ca << "]" << std::endl;
+	std::cout << "[cb: " << cb << "]" << std::endl;
+
+	ia = 5;
+	ib = 10;
+
+	fa = 10.23;
+	fb = 10.23;
+
+	ca = 'a';
+	cb = 'b';
+
+	std::cout << std::endl;
+	std::cout << "<=== Min 1/3 ===>" << std::endl;
+	std::cout << "[ia: " << ia << "]" << std::endl;
+	std::cout << "[ib: " << ib << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[The smallest number is " << min<int>(ia, ib) << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Min 2/3 ===>" << std::endl;
+	std::cout << "[fa: " << fa << "]" << std::endl;
+	std::cout << "[fb: " << fb << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[The smallest number is " << min<float>(fa, fb) << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Min 3/3 ===>" << std::endl;
+	std::cout << "[ca: " << ca << "]" << std::endl;
+	std::cout << "[cb: " << cb << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[The smallest number is " << min<char>(ca, cb) << "]" << std::endl;
+
+	ia = 5;
+	ib = 10;
+
+	fa = 10.23;
+	fb = 10.23;
+
+	ca = 'a';
+	cb = 'b';
+
+	std::cout << std::endl;
+	std::cout << "<=== Max 1/3 ===>" << std::endl;
+	std::cout << "[ia: " << ia << "]" << std::endl;
+	std::cout << "[ib: " << ib << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[The greater number is " << max<int>(ia, ib) << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Max 2/3 ===>" << std::endl;
+	std::cout << "[fa: " << fa << "]" << std::endl;
+	std::cout << "[fb: " << fb << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[The greater number is " << max<float>(fa, fb) << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Max 3/3 ===>" << std::endl;
+	std::cout << "[ca: " << ca << "]" << std::endl;
+	std::cout << "[cb: " << cb << "]" << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "[The greater number is " << max<char>(ca, cb) << "]" << std::endl;
+	return (0);
+}
---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/11 10:02:46 by gsharony          #+#    #+#             */
+/*   Updated: 2020/12/11 10:18:54 by gsharony         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T>
+void	swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T		min(T &a, T &b)
+{
+	return ((a < b) ? a : b);
+}
+
+template <typename T>
+T		max(T &a, T &b)
+{
+	return ((a > b) ? a : b);
+}
+
+#endif
