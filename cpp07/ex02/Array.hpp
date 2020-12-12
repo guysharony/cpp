46bd368cdec4318bab8818ffb49f171db3ddc240
guysharony <sharonyguy@gmail.com>
2020-12-12 11:48:56 +0100

    Finished ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,84 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 13:22:06 by gsharony          #+#    #+#             */
-/*   Updated: 2020/12/11 13:22:07 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/12/12 11:47:18 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_CLASS_HPP
+# define ARRAY_CLASS_HPP
+
+# include <iostream>
+
+template <typename T>
+class Array
+{
+	public:
+		Array(void)
+		:
+			_array(NULL),
+			_size(0)
+		{
+			return;
+		}
+		
+		Array(unsigned int n)
+		:
+			_array(new T[n]),
+			_size(n)
+		{
+			return;
+		}
+		
+		Array(Array const & src)
+		{
+			this->_array = new T[src.size()];
+			for (int i = 0; i < src.size(); i++)
+				this->_array[i] = src._array[i];
+			this->_size = src.size();
+			return;
+		}
+		
+		~Array()
+		{
+			if (this->_array != NULL)
+				delete [] this->_array;
+			return;
+		}
+
+		Array	&operator=(Array const & src)
+		{
+			this->_array = new T[src.size()];
+			for (int i = 0; i < src.size(); i++)
+				this->_array[i] = src._array[i];
+			this->_size = src.size();
+			return (*this);
+		}
+		
+		T		&operator[](unsigned int n)
+		{
+			if (n < 0 || n >= this->_size || _array == NULL)
+				throw Array::ForbiddenArray();
+			return (this->_array[n]);
+		}
+
+		int		size(void) const
+		{
+			return (this->_size);
+		}
+	
+		class ForbiddenArray : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Error: This element is out of Array memory.");
+				}
+		};
+		
+	private:
+		T		*_array;
+		int		_size;
+};
+
+#endif
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,7 +6,65 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/11 13:22:04 by gsharony          #+#    #+#             */
-/*   Updated: 2020/12/11 13:22:04 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/12/12 11:48:33 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+int		main(void)
+{
+	Array<int>				arrayInt(5);
+	Array<float>			arrayFloat(5);
+
+	Array<char>				arrayChar1(5);
+	Array<char>				arrayChar2(5);
+	Array< Array <char> >	arrayCharChar(2);
+
+	std::cout << "<=== Array Int ===>" << std::endl;
+	std::cout << "[Filling arrayInt with int from 0 to 4]" << std::endl;
+	for (int i = 0; i < 5; i++)
+		arrayInt[i] = i;
+	std::cout << "[Printing arrayInt content]" << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << "|	arrayInt[" << i << "] = " << arrayInt[i] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Array Float ===>" << std::endl;
+	std::cout << "[Filling arrayFloat with float from 0.254 to 4.254]" << std::endl;
+	for (int i = 0; i < 5; i++)
+		arrayFloat[i] = i + 0.254;
+	std::cout << "[Printing arrayFloat content]" << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << "|	arrayFloat[" << i << "] = " << arrayFloat[i] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Array Char 1 ===>" << std::endl;
+	std::cout << "[Filling arrayChar1 with char from \'a\' to \'e\']" << std::endl;
+	for (int i = 0; i < 5; i++)
+		arrayChar1[i] = 'a' + i;
+	std::cout << "[Printing arrayChar1 content]" << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << "|	arrayChar1[" << i << "] = " << arrayChar1[i] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Array Char 2 ===>" << std::endl;
+	std::cout << "[Filling arrayChar2 with char from \'f\' to \'j\']" << std::endl;
+	for (int i = 0; i < 5; i++)
+		arrayChar2[i] = 'f' + i;
+	std::cout << "[Printing arrayChar2 content]" << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << "|	arrayChar2[" << i << "] = " << arrayChar2[i] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "<=== Array Char of Array Char ===>" << std::endl;
+	std::cout << "[Filling arrayCharChar with arrayChar1 and arrayChar2]" << std::endl;
+	arrayCharChar[0] = arrayChar1;
+	arrayCharChar[1] = arrayChar2;
+	std::cout << "[Printing arrayChar content]" << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << "|	arrayCharChar[0][" << i << "] = " << arrayCharChar[0][i] << std::endl;
+	for (int i = 0; i < 5; i++)
+		std::cout << "|	arrayCharChar[1][" << i << "] = " << arrayCharChar[1][i] << std::endl;
+	return (0);
+}
