150603e42492377a254f793d2d4de8ebd4514e27
guysharony <sharonyguy@gmail.com>
2020-12-16 15:30:53 +0100

    Fixing bug

--- cpp08/ex01/span.hpp
+++ cpp08/ex01/span.hpp
@@ -6,7 +6,7 @@
 /*   By: gsharony <gsharony@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/12 14:11:02 by gsharony          #+#    #+#             */
-/*   Updated: 2020/12/16 15:27:46 by gsharony         ###   ########.fr       */
+/*   Updated: 2020/12/16 15:30:43 by gsharony         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <iostream>
 # include <vector>
+# include <algorithm>
 
 class Span
 {
